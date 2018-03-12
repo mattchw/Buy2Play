aee303daaa22830472f4d7ae2633f2fcca3d1c22
hin613 <hin_613@hotmail.com>
2018-03-12 23:22:10 +0800

    mysql database

--- server.js
+++ server.js
@@ -3,6 +3,23 @@ var app = express();
 var router = express.Router();
 var path = __dirname;
 
+var mysql = require("mysql");
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "test"
+});
+
+con.connect(function(err) {
+    if (err) {
+        console.log('mysql connecting error');
+        return;
+    }
+    console.log('mysql connecting success');
+});
+
 router.use(function (req,res,next) {
     console.log("/" + req.method);
     next();
@@ -14,8 +31,9 @@ router.get("/", function(req,res){
 
 router.get(/^[^.]+.html/, function(req, res){
     res.sendFile(path + req.url);
-    console.log(req.url);
+    console.log("You are visiting "+req.url);
 });
+
 
 app.use("/",router);
 
