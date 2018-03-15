9480f2c848eade671a853dedf0a2065bbcf5cc1d
kenlee1520 <1155079729@lnk.cuhk.edu.hk>
2018-03-15 19:01:35 +0800

    password updated

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var mysql = require("mysql");
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "",
+    password: "1520",
     database: "csci3100"
 });
 
