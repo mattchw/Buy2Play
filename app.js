274b75ef2254db20498146259a7f71fdf1c714bf
hin613 <hin_613@hotmail.com>
2018-03-16 19:37:21 +0800

    get game info from api(half success)

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ var mysql = require("mysql");
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "1520",
+    password: "",
     database: "csci3100"
 });
 
