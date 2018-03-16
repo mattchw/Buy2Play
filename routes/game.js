e1c152111f90829aad8efa3691d928e8599c03f2
kenlee1520 <1155079729@link.cuhk.edu.hk>
2018-03-17 01:34:36 +0800

    updated game search

--- routes/game.js
+++ routes/game.js
@@ -5,17 +5,20 @@ var flash    = require('connect-flash');
 var thegamesdb = require('thegamesdb');
 var app = express();
 
-var searchGame;
 //const qs = require('querystring');
 
-thegamesdb.getPlatformGames({ id: 4971 }).then(function(games){
-    console.log(games);
-    gamelist = games;
-}).catch(err => console.error(error));
+function search(gamename) {
+    //console.log("I am testing");
+    thegamesdb.getGamesList({ name: gamename }).then(function(games){
+        //console.log(games);
+        gamelist = games;
+    }).catch(err => console.error(error));
+}
 
 
 router.get('/',function(req, res, next) {  
-    
+    console.log(req.query.gamename);
+    search(req.query.gamename);
     res.render('game', { title: 'Welcome to Buy2Play', gamelist, isLoggedIn: req.isAuthenticated()});
 });
 
