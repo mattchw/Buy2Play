96584ad902f37356c7a9a814a2192111675311b2
kenlee1520 <1155079729@link.cuhk.edu.hk>
2018-03-17 00:25:22 +0800

    show platform game list

--- routes/game.js
+++ routes/game.js
@@ -8,16 +8,14 @@ var app = express();
 var searchGame;
 //const qs = require('querystring');
 
-thegamesdb.getGamesList({ name: 'marvel' }).then(function(games){
-    gamelist = games;    
+thegamesdb.getPlatformGames({ id: 25 }).then(function(games){
+    console.log(games);
+    gamelist = games;
 }).catch(err => console.error(error));
 
 
 router.get('/',function(req, res, next) {  
-    console.log(req.query.gamename);
-    searchGame = req.query.gamename;
-    console.log(searchGame);
-    console.log("---------------------------------");
+    
     res.render('game', { title: 'Welcome to Buy2Play', gamelist, isLoggedIn: req.isAuthenticated()});
 });
 
