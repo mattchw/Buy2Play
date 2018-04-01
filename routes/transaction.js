2e2a6a172fc6eb5bbb5df33b7350ce955f227648
hin613 <hin_613@hotmail.com>
2018-04-01 12:23:24 +0800

    add comments function

--- routes/transaction.js
+++ routes/transaction.js
@@ -55,6 +55,7 @@ router.get('/',isLoggedIn, function(req, res, next) {
 
 });
 
+
 router.get('/new',isLoggedIn, function(req, res, next) {
     res.render('transaction/transactionNew', { title: 'New Transaction', user:req.user, isLoggedIn: req.isAuthenticated()});
 
@@ -82,6 +83,7 @@ router.post('/add', upload.array('transactionImages', 5), function(req, res, nex
     var datetime = new Date();
     console.log(datetime);
     console.log(req.files);
+    console.log(req.body.checkbox);
     
     var str="";
     
@@ -92,6 +94,7 @@ router.post('/add', upload.array('transactionImages', 5), function(req, res, nex
         preview: req.files[0].path.substring(6),
         price: req.body.price,
         description: req.body.description,
+        location: JSON.stringify(req.body.checkbox),
         date: datetime,
         search: 0,
         likes: 0
@@ -135,7 +138,7 @@ router.get('/detail',isLoggedIn, function(req, res, next) {
     var tid = "";
     var tid = req.query.tid;
     
-    db.query('SELECT * FROM transaction T, account A, transactionImage I WHERE T.id=A.id AND T.tid=I.tid AND T.tid = ?; UPDATE transaction SET search = search+1 WHERE tid = ?; SELECT COUNT(*) AS count FROM wishlist w, transaction t WHERE w.tid=t.tid AND w.tid=? AND w.id=?; SELECT COUNT(*) AS count FROM likes l, transaction t WHERE l.tid=t.tid AND l.tid=? AND l.id=?',[tid,tid,tid,req.user.id,tid,req.user.id], function(err, rows) {
+    db.query('SELECT * FROM transaction T, account A, transactionImage I WHERE T.id=A.id AND T.tid=I.tid AND T.tid = ?; UPDATE transaction SET search = search+1 WHERE tid = ?; SELECT COUNT(*) AS count FROM wishlist w, transaction t WHERE w.tid=t.tid AND w.tid=? AND w.id=?; SELECT COUNT(*) AS count FROM likes l, transaction t WHERE l.tid=t.tid AND l.tid=? AND l.id=?; SELECT * FROM comments c, account a WHERE c.id=a.id AND c.tid=? ORDER BY c.date DESC',[tid,tid,tid,req.user.id,tid,req.user.id,tid], function(err, rows) {
         if (err) {
             console.log(err);
         }
@@ -144,8 +147,30 @@ router.get('/detail',isLoggedIn, function(req, res, next) {
         
         var checkWishlist = rows[2];
         var checkLikes = rows[3];
+        var comments=rows[4];
 
-        res.render('transaction/transactionDetail', { title: 'Transaction Detail', data: data, checkWishlist:checkWishlist, checkLikes:checkLikes, user:req.user, isLoggedIn: req.isAuthenticated()});
+        res.render('transaction/transactionDetail', { title: 'Transaction Detail', data: data, checkWishlist:checkWishlist, checkLikes:checkLikes,comments:comments, user:req.user, isLoggedIn: req.isAuthenticated()});
+    });
+
+});
+
+router.post('/comment', function(req, res, next) {
+    var db = req.con;
+    var datetime = new Date();
+    
+    var sql = {
+        tid:req.query.tid,
+        id:req.user.id,
+        date: datetime,
+        comment: req.body.comment
+    };
+    
+    var qur = db.query("INSERT INTO comments SET ?", sql, function(err, rows) {
+        if (err) {
+            console.log(err);
+        }
+        var backURL=req.header('Referer') || '/';
+        res.redirect(backURL);
     });
 
 });
