ba8230171f7fa905a156d0d4199959b69b905e75
kenlee1520 <1155079729@link.cuhk.edu.hk>
2018-04-02 16:23:41 +0800

    testing fb login

--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,27 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var flash    = require('connect-flash');
+var Strategy = require('passport-facebook').Strategy;
+
+passport.use(new Strategy({
+    clientID: '2033422926898580',
+    clientSecret: '2c57d03a3a53eaa42a729ab8e6053eca',
+    callbackURL: 'http://localhost:3000/login/facebook/return'
+},
+    function(accessToken, refreshToken, profile, cb) {
+        return cb(null, profile);
+    }
+));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+
 
 router.get('/',notLoggedIn, function(req, res) {
     res.render('login',{title: 'User Login',isLoggedIn: req.isAuthenticated(), message: req.flash('loginMessage')});
